6ed1e15d840ca0d1fa9aeaa99471358beafed0d7
Michael Tran <mltrunner@gmail.com>
2022-04-20 10:36:28 -0700

    Fixed navlinks and added card information

--- src/components/CampCard.js
+++ src/components/CampCard.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 function CampCard({site}) {
-  const {site_number, daily_price} = site
+  const {site_number, daily_price, camp_name, description, img_url} = site
   console.log("Inside CampCard: Site Number: " + site.site_number);
   console.log(site);
 
   return (
     <div className="campsite-card">
+
+      <h2> {camp_name} </h2>
+      <h2> {description} </h2>
       <h2> Site: {site_number} </h2>
       <h2> ${daily_price} per night </h2>
+      <img src={img_url}></img>
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import App from './components/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
